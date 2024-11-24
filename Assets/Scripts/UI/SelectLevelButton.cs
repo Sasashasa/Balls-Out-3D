639cd2c62edb3684579ae1e381f47b6290e3082f
Punin Aleksandr <sasashasa321@gmail.com>
2024-11-24 12:41:47 +0300

    Create level selection menu

---
+++ Assets/Scripts/UI/SelectLevelButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class SelectLevelButton : MonoBehaviour
+{
+    [SerializeField] private int _targetLevel;
+    [SerializeField] private Text _levelText;
+
+    private void Awake()
+    {
+        _levelText.text = _targetLevel.ToString();
+
+        GetComponent<Button>().onClick.AddListener(() =>
+        {
+            PlayerPrefs.SetInt("level", _targetLevel - 1);
+            SceneManager.LoadScene(1);
+        });
+    }
+}
